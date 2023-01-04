edb27ad349907baa76178662ae8f0f2ea5c627c8
belluzzojr <98968823+belluzzojr@users.noreply.github.com>
2023-01-04 16:36:07 -0300

    financeapp

--- src/Screens/Outset/Styles.tsx
+++ src/Screens/Outset/Styles.tsx
@@ -1,1 +1,51 @@
 import React from "react";
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+    container:{
+        flex: 1,
+        backgroundColor: '#201937',
+        justifyContent: 'space-evenly',
+        alignItems: 'center',
+    },
+    img:{
+        width: 400,
+        height: 266
+    },
+    title:{
+        fontFamily: 'Poppins',
+        fontWeight: '900',
+        fontSize: 52,
+
+        letterSpacing: 0.5,
+        color: '#FFFFFF'
+    },
+    subtitle:{
+        fontFamily: 'Poppins',
+        fontWeight: '400',
+        fontSize: 18,
+
+        letterSpacing: 0.5,
+        color: '#FFFFFF'
+    },
+    button:{
+        marginTop: 40,
+
+        width: 94,
+        height: 94,
+        borderRadius: 94,
+
+        backgroundColor: '#322846',
+
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    buttontext:{
+        fontFamily: 'Poppins',
+        fontWeight: '500',
+        fontSize: 14,
+        letterSpacing: 2.55,
+
+        color: '#FFFFFF'
+    }
+})
--- src/Screens/Outset/View.tsx
+++ src/Screens/Outset/View.tsx
@@ -1,13 +1,23 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { Text, View, Image, TouchableOpacity } from "react-native";
+import { styles } from "./Styles";
 
 export default function OutsetView(){
 
     return (
-        <View>
-            <Text>
-                HOME FINANCE
-            </Text>
+        <View style={styles.container}>
+            <View style={{ height: '5%'}}/>
+            <View style={{alignItems: 'center'}}>
+            <Image
+            source={require('../../../assets/utils/girloutset.png')}
+            style={styles.img}/>
+
+            <Text style={styles.title}>Finanças</Text>
+            <Text style={styles.subtitle}>Organize suas finanças</Text>
+            </View>
+            <TouchableOpacity style={styles.button}>
+                <Text style={styles.buttontext}>IR</Text>
+            </TouchableOpacity>
         </View>
     )
 }
