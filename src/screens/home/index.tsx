f0fe1a26dfb3f630d1481945d25b0d6283d6aa6a
belluzzojr <98968823+belluzzojr@users.noreply.github.com>
2023-01-06 13:23:17 -0300

    financeapp

--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -1,10 +1,29 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { View, Text} from "react-native";
+import { styles } from "./styles";
+import { TouchableOpacity } from "react-native-gesture-handler";
 
 export default function Home(){
     return (
-        <View>
-            <Text>HOME</Text>
+        <View style={styles.container}>
+            <View style={styles.containerheader}>
+            <View style={styles.alignitens}>
+            <View style={styles.containericons}>
+                <TouchableOpacity style={styles.cubeicon}/>
+                <TouchableOpacity style={styles.cubeicon}/>
+            </View>
+            
+            <View style={styles.organizetexts}>
+            <Text style={styles.title}>Seja bem-vindo </Text>
+            <Text style={styles.subtitle}>Belluzzo</Text>
+            </View>
+            </View>
+
+            <TouchableOpacity style={styles.rectanglestate}>
+                <Text style={styles.titlestate}>STATUS</Text>
+                <Text style={styles.subtitlestate}>GASTO MODERADO</Text>
+            </TouchableOpacity>
+            </View>
         </View>
     )
 }
---
+++ src/screens/home/styles.tsx
@@ -0,0 +1,78 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+    container:{
+        flex: 1,
+        backgroundColor: '#201937'
+    },
+    containerheader:{
+        flexDirection: 'row',
+        justifyContent: 'space-evenly',
+        alignItems: 'center'
+    },
+    alignitens:{
+        flexDirection: 'column'
+    },
+    containericons:{
+        flexDirection: 'row',
+        padding: 20
+    },
+    cubeicon:{
+        margin: 10,
+
+        width: 40,
+        height: 40,
+
+        borderRadius: 12,
+        borderWidth: 1,
+        borderColor: '#D0D0D0',
+
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    organizetexts:{
+        marginLeft: 30,
+    },
+    title:{
+        fontFamily: 'Poppins',
+        fontWeight: '700',
+        fontSize: 18,
+        letterSpacing: 0.5,
+
+        color: '#FFFFFF'
+    },
+    subtitle:{
+        fontFamily: 'Poppins',
+        fontWeight: '500',
+        fontSize: 14,
+
+        letterSpacing: 0.5,
+        color: '#746996'
+    },
+    rectanglestate:{
+        width: 120,
+        height: 67,
+
+        borderRadius: 12,
+        borderWidth: 1,
+        borderColor: '#D0D0D0',
+                
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    titlestate:{
+        fontFamily: 'Poppins',
+        fontWeight: '400',
+        fontSize: 18,
+
+        letterSpacing: 0.5,
+        color: '#FFFFFF'
+    },
+    subtitlestate:{
+        fontFamily: 'Poppins',
+        fontWeight: '600',
+        fontSize: 10,
+
+        color: '#AFFEAD'
+    }
+})
