1850accc360a14ecccbb70547fe48636a2c97ddd
belluzzojr <98968823+belluzzojr@users.noreply.github.com>
2023-01-06 13:24:29 -0300

    financeapp

--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -7,22 +7,26 @@ export default function Home(){
     return (
         <View style={styles.container}>
             <View style={styles.containerheader}>
+
             <View style={styles.alignitens}>
+
             <View style={styles.containericons}>
                 <TouchableOpacity style={styles.cubeicon}/>
                 <TouchableOpacity style={styles.cubeicon}/>
             </View>
             
             <View style={styles.organizetexts}>
-            <Text style={styles.title}>Seja bem-vindo </Text>
-            <Text style={styles.subtitle}>Belluzzo</Text>
+                <Text style={styles.title}>Seja bem-vindo </Text>
+                <Text style={styles.subtitle}>Belluzzo</Text>
             </View>
+
             </View>
 
             <TouchableOpacity style={styles.rectanglestate}>
                 <Text style={styles.titlestate}>STATUS</Text>
                 <Text style={styles.subtitlestate}>GASTO MODERADO</Text>
             </TouchableOpacity>
+            
             </View>
         </View>
     )
